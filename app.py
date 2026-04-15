3424f6d79359b3d59358b3efbc251961efd5aa3d
Your Name <your-email@example.com>
2026-04-15 19:16:42 +0530

    Third Commit

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 def add(a, b):
+    logging.info("Adding two numbers")
     return a + b
 
 if __name__ == "__main__":
-    print(add(2, 3))
+    result = add(2, 3)
+    logging.info(f"Result is {result}")
